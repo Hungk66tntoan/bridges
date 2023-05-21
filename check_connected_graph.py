b4a22ca0914d0697363cded29eb515de5e7274ad
phanlehoang <anthule2000@gmail.com>
2023-05-21 13:06:52 +0700

    mấy quả xử lí ảnh để đọc đc dữ liệu

--- check_connected_graph.py
+++ check_connected_graph.py
@@ -13,16 +13,13 @@ class CheckConnectedGraph:
     def union(self, u, v):
         u = self.find(u)
         v = self.find(v)
-        if u == v:
-            return
-        if self.rank[u] < self.rank[v]:
-            self.parent[u] = v
-        elif self.rank[v] < self.rank[u]:
+        if u!=v:
+            if self.rank[u] < self.rank[v]:
+                u, v = v, u
             self.parent[v] = u
-        else:
-            self.parent[v] = u
-            self.rank[u] += 1
-        self.count -= 1
+            if self.rank[u] == self.rank[v]:
+                self.rank[u] += 1
+            self.count -= 1
     def check_connected_graph(self):
         for u, v in self.edges_set:
             self.union(u, v)
